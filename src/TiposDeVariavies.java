b0d2f98cbd340731efd278acd7658661522fb923
LuanSantos26 <luansantos26@outlook.com>
2026-03-10 22:04:29 -0300

    Tipos de dados em java

---
+++ src/TiposDeVariavies.java
@@ -0,0 +1,34 @@
+public class TiposDeVariavies {
+    public static void main (String[] args){
+        // Inteiros
+        byte b = 100;
+        short s = 20000;
+        int i = 123456;
+        long l = 123456789L;
+
+        // Decimais
+        float f = 3.14f;
+        double d = 2.718281828;
+
+        // Caractere
+        char c = 'A';
+
+        // Booleano
+        boolean bool = true;
+
+        // String (referência)
+        String texto = "Olá, mundo!";
+
+        // Exibindo os valores
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + bool);
+        System.out.println("String: " + texto);
+
+    }
+}
