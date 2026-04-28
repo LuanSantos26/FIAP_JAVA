8db5ab1835cfa2ddbeaa1231e86f01c9e8654338
LuanSantos26 <luansantos26@outlook.com>
2026-04-27 22:41:39 -0300

    .

--- src/fluxoDeControle.java
+++ src/fluxoDeControle.java
@@ -1,10 +1,14 @@
 public class fluxoDeControle {
     public static void main(String[] args) {
         var a = !false ;
+
         if (a == true){
+
             System.out.println("VERDADEIRO");
+
         }else{
-        System.out.println("Falso");
+
+            System.out.println("Falso");
         }
     }
 }
