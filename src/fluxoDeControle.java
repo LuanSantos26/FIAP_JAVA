123201adaaef64e2d59868e805da5a39998068b7
LuanSantos26 <luansantos26@outlook.com>
2026-04-27 22:31:17 -0300

    Fluxo de controle com Java

---
+++ src/fluxoDeControle.java
@@ -0,0 +1,10 @@
+public class fluxoDeControle {
+    public static void main(String[] args) {
+        var a = false;
+        if (a == true){
+            System.out.println("VERDADEIRO");
+        }else{
+        System.out.println("Falso");
+        }
+    }
+}
